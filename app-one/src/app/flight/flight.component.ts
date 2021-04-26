6b613ded772ba8bf0df5efa9ab9d04c958dc7294
Mansi <Medha_Mansi@Dell.com>
2021-04-26 18:30:18 +0530

    postal.js errors

--- app-one/src/app/flight/flight.component.ts
+++ app-one/src/app/flight/flight.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import * as postal from 'postal'; //not sure about this line just trying
+export default postal;
 
 @Component({
   selector: 'app-flight',
@@ -6,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flight.component.css']
 })
 export class FlightComponent implements OnInit {
- 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private postal : postal) { 
   }
+  ngOnInit(): void {
+    var subscription = this.postal.subscribe({
+      channel : "Disable",
+      topic : "disable.button",
+      callback : function(data, envelope){
+        console.log('Hi,Message is passing');
+      }
+    });
+  }
+ 
   myClickFunction(event: any) { 
-    //just added console.log which will display the event details in browser on click of the button.
+    //just added a console.log which will display the event details in browser on click of the button.
     alert("Button is clicked");
     parent.postMessage("message","http://localhost:8082");
     console.log(event);
--- app-two/src/app/train/train.component.ts
+++ app-two/src/app/train/train.component.ts
@@ -1,19 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import  postal  from 'postal';  
 @Component({
   selector: 'app-train',
   templateUrl: './train.component.html',
   styleUrls: ['./train.component.css']
 })
 export class TrainComponent implements OnInit {
-  disableButton() {
-    // Select the element with id "theButton" from App-one inside fligth component and disable it
-    var theButton =<HTMLInputElement>document.getElementById("theButton");
-    theButton.disabled=true;
-  }
   
-  constructor() { }
+  ButtonCall(){
+      this.postal.publish({
+          channel : "Disbale",
+          topic : "disbale.button",
+          data : {
+                disbaleButton : true
+          }
+      });
+  }
+  // disableButton(buttonid) {
+  //   // Select the element with id "theButton" from App-one inside fligth component and disable it
+  //   var theButton =<HTMLInputElement>document.getElementById(buttonid);
+  //   alert(buttonid);
+  //   theButton.setAttribute('disabled','true');
+  // }
+  constructor(private postal : postal) {  }
 
   ngOnInit(): void {
 }
+
 }
+
+
+
+
